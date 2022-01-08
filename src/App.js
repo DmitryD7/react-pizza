1c3f690984c1150bbe437931417c8253af87082e
DmitryDemid <dimon.demid15@gmail.com>
2022-01-08 00:52:00 -0500

    installing and using axios and getting data from db

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import './scss/app.scss';
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Header} from "./components";
 import {Routes, Route} from "react-router-dom";
 import {Home, Cart} from "./pages";
+import axios from "axios";
 
 
 function App() {
+    const [pizzasState, setPizzasState] = useState([]);
+    useEffect(()=>{
+        axios.get('http://localhost:3000/db.json').then(res => {
+            setPizzasState(res.data.pizzas)
+        });
+    },[]);
+
+
     return (
         <div className="App">
             <div className="wrapper">
                 <Header/>
                 <div className="content">
                     <Routes>
-                        <Route exact path={'/'} element={<Home/>}/>
+                        <Route exact path={'/'} element={<Home pizzasState={pizzasState}/>}/>
                         <Route path={'/cart'} element={<Cart/>}/>
                     </Routes>
                 </div>
